926428c8bc34cdefc5a3ee8fdd0649507ebe8d68
chenrongguang <89261432@qq.com>
2016-10-09 21:19:14 +0800

    验证码格式

--- sourcecode/sanheyi/app/Home/Controller/LoginController.class.php
+++ sourcecode/sanheyi/app/Home/Controller/LoginController.class.php
@@ -72,8 +72,8 @@ class LoginController extends Controller
     {
         $config = array(
             'fontSize' => 10,              // 验证码字体大小(px)
-            'useCurve' => true,            // 是否画混淆曲线
-            'useNoise' => true,            // 是否添加杂点
+            'useCurve' => false,            // 是否画混淆曲线
+            'useNoise' => false,            // 是否添加杂点
             'imageH' => 35,               // 验证码图片高度
             'imageW' => 80,               // 验证码图片宽度
             'length' => 4,               // 验证码位数
